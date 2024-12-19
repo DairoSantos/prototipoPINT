52be6d0b104c1d2019f4855f50f69291e46db8fe
Dairo <dairoestebansantos@gmail.com>
2024-12-19 01:14:21 +0100

    Añadiendo rutas
    No se me ocurren, así que estoy  poniendo un poco de relleno

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,23 @@ router.get("/rutaGet", (req, res) => {
     res.send("hola desde una web llamada en el servidor");
 });
 
+router.get("/1", (req, res) => {
+    res.send("hola");
+});
+
+router.get("/2", (req, res) => {
+    res.send("mundo");
+});
+
+router.get("/3", (req, res) => {
+    res.send("desde el");
+});
+
+router.get("/3", (req, res) => {
+    res.send("servidor");
+});
+
+router.get("/4", (req, res) => {
+    res.send("No se si esto vale como 5 rutas diferentes, pero como no se me ocurria nada mas he hecho esto");
+});
 module.exports = router;

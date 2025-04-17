be3a1b443394f8f177b52a99752d29fca1d42799
Dairo <dairoestebansantos@gmail.com>
2025-04-17 17:36:56 +0200

    ACTUALIZANDO

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -30,16 +30,16 @@ router.post("/rutapost", (req, res) => {
     console.log(req.body.nacionalidad);
     console.log(req.body.sexo);
 
-    //res.send("Datos recibidos");
-    console.log("He entrado a la post");
-    res.json({
-       nombre: req.body.nombre,
-       apellido: req.body.apellido,
-       edad: req.body.edad,
-       nacionalidad: req.body.nacionalidad,
-       sexo: req.body.sexo,
-    
-    })
+    res.send("Datos recibidos");
+    //console.log("He entrado a la post");
+    //res.json({
+    //   nombre: req.body.nombre,
+    //   apellido: req.body.apellido,
+    //   edad: req.body.edad,
+    //   nacionalidad: req.body.nacionalidad,
+    //   sexo: req.body.sexo,
+    //
+    //})
     //res.redirect("http://localhost:8080/rutapost");
 });
 

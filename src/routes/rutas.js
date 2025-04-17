367ea2d8e9495643feb102d1a93ba8e8c837d987
Dairo <dairoestebansantos@gmail.com>
2025-04-17 17:19:45 +0200

    ordenando rutas

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -2,8 +2,23 @@ const { Router } = require("express");
 const router = Router();
 
 //Ruta GET
-router.get("/rutaGetConRouter",  (req,res) => {
-    res.send("Hola desde una web llamada en el servidor utilizando routes")
+router.get("/rutaget",  (req,res) => {
+    console.log(req.body.nombre);
+    console.log(req.body.apellido);
+    console.log(req.body.edad);
+    console.log(req.body.nacionalidad);
+    console.log(req.body.sexo);
+
+    //res.send("Datos recibidos");
+    console.log("He entrado a la post");
+    res.json({
+       nombre: req.body.nombre,
+       apellido: req.body.apellido,
+       edad: req.body.edad,
+       nacionalidad: req.body.nacionalidad,
+       sexo: req.body.sexo,
+    
+    })
 });
 
 //Ruta POST

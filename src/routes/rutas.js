66602603a3bf09089c5809d5362de37846d5787e
Dairo <dairoestebansantos@gmail.com>
2025-04-20 10:52:34 +0200

    agregando rutas para pi-008

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 
-//Ruta GET
+//Rutas GET
 router.get("/rutaget",  (req,res) => {
     console.log(req.body.nombre);
     console.log(req.body.apellido);
@@ -10,11 +10,32 @@ router.get("/rutaget",  (req,res) => {
     console.log(req.body.sexo);
 
     res.send("Datos recibidos");
-    console.log("He entrado a la post");
+    console.log("He entrado a la ruta get");
     
 });
+router.get("/rutaget2",  (req,res) => {
+    res.send("probando ruta2")
+});
+router.get("/rutaget3", (req, res) => {
+    console.log("Probando enlaces");
+    res.send("https://www.youtube.com/watch?v=1vbZMpRTT5M");
+    console.log("He entrado a la ruta get");
+});
 
-//Ruta POST
+router.get("/rutaget4", (req, res) => {
+    console.log("Probando enlace con JSON");
+    res.json({
+        link: "https://www.youtube.com/watch?v=1vbZMpRTT5M"
+    });
+    console.log("He entrado a la ruta get");
+});
+router.get("/rutaget5", (req, res) => {
+    console.log("Probando ruta igual");
+    res.send("https://www.youtube.com/watch?v=1vbZMpRTT5M");
+    console.log("He entrado a la ruta get");
+});
+
+//Rutas POST
 
 router.post("/rutapost", (req, res) => {
     console.log(req.body.nombre);
@@ -35,7 +56,22 @@ router.post("/rutapost", (req, res) => {
     //})
     //res.redirect("http://localhost:8080/rutapost");
 });
-
+router.post("/rutapost2", (req, res) => {
+    console.log(" probando ruta vacia ");
+    res.send("");
+    console.log("He entrado a la ruta post");
+});
+router.post("/rutapost3", (req, res) => {
+    console.log(" probando enlace en POST");
+    res.send("https://www.youtube.com/watch?v=1vbZMpRTT5M");
+    console.log("He entrado a la ruta post");
+});
+router.post("/rutapost4", (req, res) => {
+    console.log("Probando enlace con JSON en POST");
+    res.json({
+        link: "https://www.youtube.com/watch?v=1vbZMpRTT5M"
+    });
+});
 
 
 module.exports = router;
